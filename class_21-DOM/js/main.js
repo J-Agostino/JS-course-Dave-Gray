40ac9a55de099a6b773eee290f2fcad2adce085a
J-Agostino <agostino.jpa@gmail.com>
2023-11-01 21:59:28 -0300

    sort update

---
+++ class_21-DOM/js/main.js
@@ -0,0 +1,13 @@
+// DOM - Document Object Model
+
+// SELECT BY ID
+const view1 = document.getElementById("view1");
+console.log(view1);
+
+const view2 = document.querySelector("#view2")
+console.log(view2)
+
+
+// CHANGE THE STYLE
+view1.style.display = "none"
+view2.style.display = "flex"
